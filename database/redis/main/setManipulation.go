d829c8849c87dab281714bca6b84c495d68997a6
vya <yavorskyyval@gmail.com>
2017-08-30 23:30:57 +0300

    training with Redis and Postgres

--- database/redis/main/setManipulation.go
+++ database/redis/main/setManipulation.go
@@ -3,13 +3,14 @@ package main
 import (
 	"github.com/bohdanlisovskyi/Golang-252-Telegram-Bot-Game/database/redis"
 	"fmt"
+	"github.com/8tomat8/SSU-Golang-252-Chat/loger"
 )
 
 func main() {
 
 	conn, err := redis.GetRedisConn()
 	if err != nil {
-
+		loger.Log.Errorf("error")
 	}
 
 	s, err := conn.Cmd("SADD", "authos", "zaz", "bmw", "toyota").Str()
